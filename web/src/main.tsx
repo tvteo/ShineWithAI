990d0de2fa8c05d0452d24a8727a55086cd6639e
tbkhiem2015 <tbkhiem2015@gmail.com>
2025-08-19 16:03:54 +0700

    split web api

---
+++ web/src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import App from "./App";
+import "./index.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql", // API NestJS GraphQL
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode>
+);
---
+++ web/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Xin chào 👋</h1>
+      <p className="mt-2 text-gray-700">
+        Nội dung này chiếm toàn bộ phần còn lại của màn hình (100% chiều cao).
+      </p>
+    </div>
+  );
+};
+
+export default Home;
